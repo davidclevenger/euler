fc72469a9f0929036de957206cb13e9e87a375a5
David Clevenger <dclevenger00@gmail.com>
2021-05-26 23:57:00 -0500

    Problem 1 and problem 4

---
+++ problems/p1.ml
@@ -0,0 +1,21 @@
+(* problem 1 - author: davidclevenger *)
+
+let rec range a b =
+	if a > b then []
+	else a :: (range (a+1) b)
+
+let filt i = 
+	if i mod 3 = 0 || i mod 5 = 0 then i
+	else 0 
+
+let rec sum l = match l with
+	[] -> 0
+	| h :: t -> h + (sum t)
+
+(* range function is inclusive and problems states the domain is below 1000 *)
+let base = range 1 999
+
+let filtered = List.map filt base
+let solution = sum filtered
+
+let () = print_endline(string_of_int(solution));
---
+++ problems/p4.ml
@@ -0,0 +1,23 @@
+open Base;;
+
+let is_palindrome s = String.equal s (String.rev s);;
+
+let result = 
+    let longest = ref 0 in
+        (* Since we are looking for the longest, *
+         * start with the largest indices        *)
+        for i = 999 downto 900 do
+            for j = 999 downto 900 do
+                let prod = i * j in
+                let s = Int.to_string(prod) in
+                    longest := if is_palindrome s 
+                            && String.length s > String.length (Int.to_string(!longest)) then 
+                        prod
+                    else 
+                        !longest
+            done;
+        done;
+!longest
+;;
+
+let () = Stdio.print_endline(Int.to_string(result));;
